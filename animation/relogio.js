7407a8ffd26b5b154d2ce5ca1affce02046b6fa7
daryud <darinformax@gmail.com>
2022-03-30 22:38:14 -0300

    30/03/2022 - relógio ajustado

--- animation/relogio.js
+++ animation/relogio.js
@@ -4,21 +4,16 @@ const hora = document.querySelector("#hr");
 const minuto = document.querySelector("#min");
 const segundo = document.querySelector("#seg");
 
-hora.value = 1;
-minuto.value = 1;
-segundo.value = 1;
-
 setInterval(() => {
-  segundo.style.transform = `rotateZ(${deg * segundo.value}deg)`;
-  segundo.value++;
-}, 1000);
+  const date = new Date();
 
-setInterval(() => {
+  hora.value = date.getHours();
+  minuto.value = date.getMinutes();
+  segundo.value = date.getSeconds();
+
+  hora.style.transform = `rotateZ(${
+    degHora * hora.value + minuto.value / 12
+  }deg)`;
   minuto.style.transform = `rotateZ(${deg * minuto.value}deg)`;
-  minuto.value++;
-}, 60 * 1000);
-
-setInterval(() => {
-  hora.style.transform = `rotateZ(${degHora * hora.value}deg)`;
-  hora.value++;
-}, 60 * 60 * 1000);
+  segundo.style.transform = `rotateZ(${deg * segundo.value}deg)`;
+}, 1000);
